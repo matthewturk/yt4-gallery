194ee8367db1fb5af83084f9bfbb44d77cbd09f6
Matthew Turk <matthewturk@gmail.com>
2020-06-15 18:15:16 -0500

    update

--- generate_scripts.py
+++ generate_scripts.py
@@ -4,6 +4,18 @@ import sys
 import os
 import json
 import requests
+from filelock import Timeout, FileLock
+
+# datasets that we usually generate this with:
+#
+#  * gizmo_64
+#  * IsothermalCollapse
+#  * snapshot_033
+#  * ArepoBullet (doesn't work on yt3)
+#  * snipshot_399_z000p000
+#  * GadgetDiskGalaxy
+#  * halo1e11_run1.00400
+#  * TipsyGalaxy
 
 YT3_PATH = os.path.expanduser("~/yt/yt-3.x-clean/")
 YT4_PATH = os.path.expanduser("~/yt/yt/")
@@ -29,26 +41,33 @@ def list_datafiles():
             fn = ds
         click.echo(f"{fn}");
 
+
+lock = FileLock("catalog.json.lock")
+
 def append_saved_file(dataset, version, info):
-    if os.path.isfile("catalog.json"):
-        current = json.load(open("catalog.json", "r"))
-    else:
-        current = {}
-    current.setdefault(dataset, {}).setdefault(version, []).append(info)
-    json.dump(current, open("catalog.json", "w"))
+    with lock:
+        if os.path.isfile("catalog.json"):
+            current = json.load(open("catalog.json", "r"))
+        else:
+            current = {}
+        current.setdefault(dataset, {}).setdefault(version, []).append(info)
+        json.dump(current, open("catalog.json", "w"), indent=2)
 
 @main.command()
+@click.option("-c", "--center-max", required=False, default=False, type=bool)
 @click.option("-y", "--yt-version", required=False, default=4, type=int)
 @click.argument("dataset", type=str, required=True)
-def make_plots(dataset, yt_version):
+def make_plots(dataset, yt_version, center_max):
+    catalog = _get_datafiles()
+    kwargs = catalog[dataset + '.tar.gz'].get("load_kwargs", {})
     if yt_version == 3:
         sys.path.insert(0, YT3_PATH)
         field_key = 'yt4'
         #weight_default = ("deposit", "gas_density")
         weight_default = ("gas", "density")
         import yt
-        catalog = _get_datafiles()
-        ds = yt.load(f"{dataset}.tar.gz.untar/{dataset}/{catalog[dataset + '.tar.gz']['load_name']}")
+        click.echo(yt)
+        ds = yt.load(f"{dataset}.tar.gz.untar/{dataset}/{catalog[dataset + '.tar.gz']['load_name']}", **kwargs)
     elif yt_version == 4:
         sys.path.insert(0, YT4_PATH)
         field_key = 'yt4'
@@ -74,7 +93,11 @@ def make_plots(dataset, yt_version):
                 w = None
                 wn = "None"
             for ax in field_spec['axis']:
-                p = yt.ProjectionPlot(ds, ax, f, weight_field = w)
+                if center_max:
+                    center = "max"
+                else:
+                    center = "c"
+                p = yt.ProjectionPlot(ds, ax, f, weight_field = w, center = center)
                 for zoom in field_spec['zoom']:
                     p.set_width(1.0, 'unitary')
                     p.zoom(zoom)
@@ -85,7 +108,7 @@ def make_plots(dataset, yt_version):
                     append_saved_file(dataset, f"yt{yt_version}", 
                                       {'field_key': f"{f[1]}_{wn}", 'axis': ax, 
                                        'zoom': zoom, 'filename': fn,
-                                       'plot_index': plot_index})
+                                       'plot_index': plot_index, 'center_max': center_max})
                     plot_index += 1
 
 if __name__ == "__main__":
